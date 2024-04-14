1cfec9941ea412cea1c96129b4b9bceccc7deab4
RodrigoDosSantos <rodrigosantospx@gmail.com>
2024-04-14 18:30:47 -0300

    Conclusão desafio curso 3 aula 1

---
+++ Java/aluraCurso/src/main/java/com/alura/curso3ColecoesDeDadosEListas/Desafios/DesafioAula1/Main.java
@@ -0,0 +1,26 @@
+package com.alura.curso3ColecoesDeDadosEListas.Desafios.DesafioAula1;
+
+import com.alura.curso3ColecoesDeDadosEListas.Desafios.DesafioAula1.model.Pessoa;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        Pessoa pessoa = new Pessoa();
+        ArrayList<Pessoa> listaDePessoas = new ArrayList<>();
+        pessoa.setIdade(20);
+        pessoa.setNome("Rodrigo");
+        Pessoa pessoa2 = new Pessoa();
+        pessoa2.setIdade(10);
+        pessoa2.setNome("Roberto");
+        Pessoa pessoa3 = new Pessoa();
+        pessoa3.setIdade(15);
+        pessoa3.setNome("Kiara");
+        listaDePessoas.add(pessoa);
+        listaDePessoas.add(pessoa2);
+        listaDePessoas.add(pessoa3);
+        System.out.println("Tamanho da lista: " + listaDePessoas.size());
+        System.out.println("Primeira pessoa da lista: " + listaDePessoas.get(0));
+        System.out.println(listaDePessoas);
+    }
+}
---
+++ Java/aluraCurso/src/main/java/com/alura/curso3ColecoesDeDadosEListas/Desafios/DesafioAula1/model/Pessoa.java
@@ -0,0 +1,29 @@
+package com.alura.curso3ColecoesDeDadosEListas.Desafios.DesafioAula1.model;
+
+public class Pessoa {
+    String nome;
+    int idade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    @Override
+    public String toString() {
+        return
+                "{" + "nome:" + nome +
+                ",idade:" + idade + "}";
+    }
+}
